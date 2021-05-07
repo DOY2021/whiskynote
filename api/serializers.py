6db7896a64f55d1ae120765f91486a084f43fb33
sunnyw <vnvnchlrh@gmail.com>
2021-05-07 13:49:50 +0900

    Sunwoo-PasswordResetCompleted

--- api/serializers.py
+++ api/serializers.py
@@ -102,51 +102,53 @@ class PasswordResetConfirmSerializer(serializers.Serializer):
     """
     new_password1 = serializers.CharField(max_length=128)
     new_password2 = serializers.CharField(max_length=128)
-    #uid = serializers.CharField()
-    #token = serializers.CharField()
+    uid = serializers.CharField()
+    #Front url parameter <uid>
+    token = serializers.CharField()
+    #Front url parameter <token>
 
     set_password_form_class = SetPasswordForm
 
     def custom_validation(self, attrs):
         pass
 
-    def validate(self, attrs):
-        self._errors = {}
-        request_object = self.context['request']
-        uidb64 = request_object.query_params.get('uidb64')
-        uid = force_text(uid_decoder('Mg'))
-        #retrieve uid info in 'MQ' space
-        self.user = UserModel._default_manager.get(pk=uid)
-
-        self.set_password_form = self.set_password_form_class(
-                user=self.user, data=attrs
-                )
-        if not self.set_password_form.is_valid():
-            raise serializers.ValidationError(self.set_password_form.error)
-
-        return attrs
-
     #def validate(self, attrs):
     #    self._errors = {}
+    #    request_object = self.context['request']
+    #    uidb64 = request_object.query_params.get('uidb64')
+    #    uid = force_text(uid_decoder('Mg'))
+    #    #retrieve uid info in 'MQ' space
+    #    self.user = UserModel._default_manager.get(pk=uid)
 
-    #    # Decode the uidb64 to uid to get User object
-    #    try:
-    #        uid = force_text(uid_decoder(attrs['uid']))
-    #        self.user = UserModel._default_manager.get(pk=uid)
-    #    except (TypeError, ValueError, OverflowError, UserModel.DoesNotExist):
-    #        raise ValidationError({'uid': ['Invalid value']})
-
-    #    self.custom_validation(attrs)
-    #    # Construct SetPasswordForm instance
     #    self.set_password_form = self.set_password_form_class(
-    #        user=self.user, data=attrs
-    #    )
+    #            user=self.user, data=attrs
+    #            )
     #    if not self.set_password_form.is_valid():
-    #        raise serializers.ValidationError(self.set_password_form.errors)
-    #    if not default_token_generator.check_token(self.user, attrs['token']):
-    #        raise ValidationError({'token': ['Invalid value']})
+    #        raise serializers.ValidationError(self.set_password_form.error)
 
     #    return attrs
+
+    def validate(self, attrs):
+        self._errors = {}
+
+        # Decode the uidb64 to uid to get User object
+        try:
+            uid = force_text(uid_decoder(attrs['uid']))
+            self.user = UserModel._default_manager.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, UserModel.DoesNotExist):
+            raise ValidationError({'uid': ['Invalid value']})
+
+        self.custom_validation(attrs)
+        # Construct SetPasswordForm instance
+        self.set_password_form = self.set_password_form_class(
+            user=self.user, data=attrs
+        )
+        if not self.set_password_form.is_valid():
+            raise serializers.ValidationError(self.set_password_form.errors)
+        if not default_token_generator.check_token(self.user, attrs['token']):
+            raise ValidationError({'token': ['Invalid value']})
+
+        return attrs
 
     def save(self):
         return self.set_password_form.save()
