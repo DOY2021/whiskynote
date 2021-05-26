f74706e54d727b77d37886971a9707ac8afca15c
janeshin059 <janeshin059@gmail.com>
2021-05-26 17:19:45 +0900

    Header code refactoring

--- Front/src/Feature/Header/Header.tsx
+++ Front/src/Feature/Header/Header.tsx
@@ -5,62 +5,8 @@ import Button from '../../shared/Button/Button';
 import { Link, useHistory } from 'react-router-dom';
 import Palette from '../../css/Palette';
 import { useCookies } from 'react-cookie';
-
+import S from './Header.styled';
 import HeaderMenuList from './HeaderMenuList';
-
-
-const Nav = styled.div`
-  background-color: white;
-`;
-const NavHeader = styled.div`
-  align-items: center;
-  justify-items: center;
-  display: flex;
-  width: 100%;
-`;
-const NavLeft = styled.div`
-  width: 20%;
-  align-items: center;
-  text-align: center;
-  cursor: pointer;
-`;
-const NavCenter = styled.div`
-  display: inline-flex;
-  justify-content: center;
-  cursor: pointer;
-  align-items: center;
-  text-align: center;
-`;
-
-const NavRight = styled.div`
-  width: 20%;
-  text-align: center;
-`;
-
-const MenuLink = styled(Link)`
-  font-size: 16px;
-  color: #495057;
-  font-family: 'Noto Sans KR', sans-serif;
-
-  width: 100px;
-  height: 60px;
-  display: inline-flex;
-  justify-content: center;
-  cursor: pointer;
-  align-items: center;
-  text-align: center;
-
-  &:hover {
-    color: ${Palette.YB600};
-  }
-  &:active {
-    color: ${Palette.YB500};
-  }
-`;
-
-const SearchInput = styled.input`
-  background-color: white;
-`;
 
 const SearchIcon = styled(FaSearch)`
   color: ${Palette.Gray500};
@@ -68,35 +14,29 @@ const SearchIcon = styled(FaSearch)`
   margin-left: 25px;
 `;
 
-const MenuMargin = styled.div`
-  margin-right: 4px;
-`;
-
-
 function Header() {
   const [cookie] = useCookies(['user_id']);
 
-
   return (
-    <Nav>
-      <NavHeader>
-        <NavLeft>
+    <S.Nav>
+      <S.NavHeader>
+        <S.NavLeft>
           <Link to="/">
             <img src={'../../../assets/logo/logo.svg'}></img>
           </Link>
-        </NavLeft>
-        <NavCenter>
-          <MenuLink to="#">위스키</MenuLink>
-          <MenuMargin></MenuMargin>
-          <MenuLink to="#">위스키 바</MenuLink>
-          <MenuMargin></MenuMargin>
-          <MenuLink to="#">커뮤니티</MenuLink>
-          <MenuMargin></MenuMargin>
-          <MenuLink to="#">서비스 소개</MenuLink>
+        </S.NavLeft>
+        <S.NavCenter>
+          <S.MenuLink to="#">위스키</S.MenuLink>
+          <S.MenuMargin></S.MenuMargin>
+          <S.MenuLink to="#">위스키 바</S.MenuLink>
+          <S.MenuMargin></S.MenuMargin>
+          <S.MenuLink to="#">커뮤니티</S.MenuLink>
+          <S.MenuMargin></S.MenuMargin>
+          <S.MenuLink to="#">서비스 소개</S.MenuLink>
           <SearchIcon></SearchIcon>
-          <SearchInput></SearchInput>
-        </NavCenter>
-        <NavRight>
+          <S.SearchInput></S.SearchInput>
+        </S.NavCenter>
+        <S.NavRight>
           {!cookie.user_id && (
             <>
               <Link to="/signup">
@@ -114,9 +54,9 @@ function Header() {
           {cookie.user_id && (
             <HeaderMenuList></HeaderMenuList>
           )}
-        </NavRight>
-      </NavHeader>
-    </Nav>
+        </S.NavRight>
+      </S.NavHeader>
+    </S.Nav>
   );
 }
 
