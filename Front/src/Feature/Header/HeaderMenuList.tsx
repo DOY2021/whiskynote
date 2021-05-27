ac964415a538cbf71362015a5b4fb1c81eef1784
janeshin059 <janeshin059@gmail.com>
2021-05-27 21:40:41 +0900

    Display Profile Image

--- Front/src/Feature/Header/HeaderMenuList.tsx
+++ Front/src/Feature/Header/HeaderMenuList.tsx
@@ -5,77 +5,8 @@ import styled, { css, keyframes } from 'styled-components';
 import { authAPI } from '../../api/auth';
 import Palette from '../../css/Palette';
 import { useCookies } from 'react-cookie';
-
-type activeProp = {
-  isActive: boolean;
-};
-
-const trans = keyframes`
-  from {
-    transForm: translateY(-10px);
-  }
-  to {
-    transForm: translateY(0);
-  }
-`;
-
-const slideIn = keyframes`
-  from {
-    opacity: 0;
-  }
-
-  to {
-    opacity: 1;
-  }
-`;
-
-const MenuNav = styled.nav<activeProp>`
-  position: absolute;
-  top: 50px;
-  width: 176px;
-  height: 176px;
-  filter: drop-shadow(0px 0px 20px rgba(0, 0, 0, 0.1));
-  border-radius: 8px;
-  background-color: white;
-  right: 0px;
-  visibility: hidden;
-  animation: ${slideIn} 0.4s ease, ${trans} 0.4s ease, visibility 0.4s;
-
-  ${props =>
-    props.isActive &&
-    `
-        visibility: visible;
-  `}
-`;
-
-const MenuFont = styled.div`
-  font-size: 16px;
-  color: ${Palette.Gray900};
-  margin-left: 10px;
-`;
-
-const MenuItem = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  padding: 12px;
-  &:hover {
-    background-color: ${Palette.Gray000};
-  }
-`;
-
-const PositionMenuList = styled.div`
-  position: relative;
-`;
-
-const UserIcon = styled.div`
-  width: 48px;
-  height: 48px;
-  border-radius: 50%;
-  margin-top: 10px;
-  float: right;
-  cursor: pointer;
-`;
+import { useUserState } from '../../hook/useUserContext';
+import S from './HeaderMenuList.styled';
 
 const userIconStyle = {
   color: Palette.Gray500,
@@ -86,6 +17,7 @@ const userIconStyle = {
 const IconStyle = { fontSize: '20px', color: `${Palette.YB400}` };
 
 function HeaderMenuList(props) {
+  const user = useUserState();
   const history = useHistory();
   const dropdownRef = useRef(null);
   const [isActive, setIsActive] = useState(false);
@@ -93,9 +25,8 @@ function HeaderMenuList(props) {
   const [cookies, setCookie, removeCookie] = useCookies(['user_id']);
   const onDropdownClick = () => setIsActive(!isActive);
 
-  console.log(isActive);
+  //console.log(isActive);
   useEffect(() => {
-    //component mounted
     const pageClickEvent = e => {
       if (dropdownRef.current !== null) {
         setIsActive(!isActive);
@@ -106,7 +37,6 @@ function HeaderMenuList(props) {
       window.addEventListener('click', pageClickEvent);
     }
     return () => {
-      //component unmounted
       window.removeEventListener('click', pageClickEvent);
     };
   }, [isActive]);
@@ -122,37 +52,41 @@ function HeaderMenuList(props) {
   };
 
   return (
-    <PositionMenuList>
-      <UserIcon>
+    <S.PositionMenuList>
+      {user?.profile_photo ? <S.ProfileImg src={user.profile_photo}>
+      </S.ProfileImg> :
+      <S.UserIcon>
         <FaRegUser size={25} style={userIconStyle} onClick={onDropdownClick} />
-      </UserIcon>
+      </S.UserIcon>
+       }
+      
       {isActive && (
-        <MenuNav ref={dropdownRef} isActive={isActive}>
+        <S.MenuNav ref={dropdownRef} isActive={isActive}>
           <Link to="/my-page">
-            <MenuItem>
+            <S.MenuItem>
               <FaRegUser style={IconStyle}></FaRegUser>
-              <MenuFont>마이페이지</MenuFont>
-            </MenuItem>
+              <S.MenuFont>마이페이지</S.MenuFont>
+            </S.MenuItem>
           </Link>
           <Link to="/alerts">
-            <MenuItem>
+            <S.MenuItem>
               <FaRegBell style={IconStyle}></FaRegBell>
-              <MenuFont>알림</MenuFont>
-            </MenuItem>
+              <S.MenuFont>알림</S.MenuFont>
+            </S.MenuItem>
           </Link>
           <Link to="/settings">
-            <MenuItem>
+            <S.MenuItem>
               <FaCog style={IconStyle}></FaCog>
-              <MenuFont>설정</MenuFont>
-            </MenuItem>
+              <S.MenuFont>설정</S.MenuFont>
+            </S.MenuItem>
           </Link>
-          <MenuItem onClick={onLogOut}>
+          <S.MenuItem onClick={onLogOut}>
             <FaSignOutAlt style={IconStyle}></FaSignOutAlt>
-            <MenuFont>로그아웃</MenuFont>
-          </MenuItem>
-        </MenuNav>
+            <S.MenuFont>로그아웃</S.MenuFont>
+          </S.MenuItem>
+        </S.MenuNav>
       )}
-    </PositionMenuList>
+    </S.PositionMenuList>
   );
 }
 
