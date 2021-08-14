5c6ddcdcb9dbb9e0d34e0ac3288b6c13627b023b
janeshin059 <janeshin059@gmail.com>
2021-08-14 19:46:13 +0900

    Header&landing

--- Front/src/Feature/Landing/Landing.tsx
+++ Front/src/Feature/Landing/Landing.tsx
@@ -1,9 +1,13 @@
 
 import React from 'react';
+import Explore from '../Explore/Explore';
+import NewWhiskyReview from '../Review/NewWhiskyReview/NewWhiskyReview';
+
 function Landing(){
-  return(<div>this is landdzzing page 
-    {/* <NewWhiskyReview></NewWhiskyReview> */}
-    {/* <ReviewList></ReviewList> */}
+  return(<div>
+    <NewWhiskyReview></NewWhiskyReview>
+    {/* <DB></DB> */}
+
   </div>)
 }
 export default Landing;
