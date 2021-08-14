87e4033d1ff4586611c540a49f99ae89cee3e4e0
kyeongsoosoo <shakramon@naver.com>
2021-08-14 22:20:18 +0900

    Test

--- Front/config/webpack.common.js
+++ Front/config/webpack.common.js
@@ -14,6 +14,14 @@ module.exports = {
         exclude: /(node_modules)/,
         use: {
           loader:'babel-loader',
+          options: {
+              "presets": [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript"
+              ],
+              "plugins": ["babel-plugin-styled-components","@babel/plugin-syntax-jsx"]
+          }
         }
       },
       {
