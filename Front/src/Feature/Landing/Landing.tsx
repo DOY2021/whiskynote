754ce4678ad71f4314c557ec2cecb4fea65280ea
janeshin059 <janeshin059@gmail.com>
2021-08-14 19:46:13 +0900

    Header&landing

--- Front/src/Feature/Landing/Landing.tsx
+++ Front/src/Feature/Landing/Landing.tsx
@@ -6,11 +6,13 @@ import ReviewInput, { ReviewType } from '../Review/ReviewInput/ReviewInput';
 import Review from '../Review/ReviewListing/Review/Review';
 import WhiskyNoteReview from '../Review/ReviewListing/Review/WhiskyNoteReview';
 import ReviewList from '../Review/ReviewListing/ReviewList/ReviewList';
+import DB from '../WhiskyDB/DB';
 
 function Landing(){
-  return(<div>this is landing page 
- {/* <NewWhiskyReview></NewWhiskyReview> */}
-    {/* <ReviewList></ReviewList> */}
+  return(<div>
+ <NewWhiskyReview></NewWhiskyReview>
+ {/* <DB></DB> */}
+
   </div>)
 }
 export default Landing;
