6b39af84ea02166f5526c3ae2325f60de8af687e
janeshin059 <janeshin059@gmail.com>
2021-08-14 14:06:35 +0900

    Header layout using bootstrap

--- Front/src/Feature/Header/Header.styled.ts
+++ Front/src/Feature/Header/Header.styled.ts
@@ -1,15 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const Nav = styled.div`
-  background-color: #EDECE6;
-  min-width: 1200px;
-  width: 100vw;
-  padding: 20px 50px;
-  margin-bottom: 50px;
-  border-bottom: 1px solid #736C64;
-`;
-
 const MenuBtn = styled.button`
   background-color: transparent;
   cursor: pointer;
@@ -18,54 +9,18 @@ const MenuBtn = styled.button`
   display: inline-flex;
   justify-content: center;
   text-align: center;
-  margin-right:10px;
+  margin-right: 10px;
 `;
 const SignUpBtn = styled.div`
   background-color: transparent;
   cursor: pointer;
-  color: #201F1E;
+  color: #201f1e;
   font-size: 16px;
   display: inline-flex;
   justify-content: center;
   text-align: center;
   border: 1px solid #272624;
   padding: 7px;
-`;
-const NavHeader = styled.div`
-  align-items: center;
-  justify-content: space-between;
-  display: flex;
-  width: 100%;
-  margin:0 auto;
-`;
-
-const NavLeftContent = styled.div`
-  display:flex;
-  justify-content: space-around;
-  align-items: center;
-
-  width: 600px;
-  height: 100%;
-
-`;
-
-const NavLeft = styled.div`
-  width: 20%;
-  align-items: center;
-  text-align: center;
-  cursor: pointer;
-`;
-const NavCenter = styled.div`
-  display: inline-flex;
-  justify-content: center;
-  cursor: pointer;
-  align-items: center;
-  text-align: center;
-`;
-
-const NavRight = styled.div`
-  width: 60%;
-  text-align: center;
 `;
 
 const MenuLink = styled(Link)`
@@ -93,12 +48,6 @@ const MenuMargin = styled.div`
 `;
 
 export default {
-  Nav,
-  NavCenter,
-  NavHeader,
-  NavLeft,
-  NavLeftContent,
-  NavRight,
   MenuLink,
   SearchInput,
   MenuMargin,
--- Front/src/Feature/Header/Header.tsx
+++ Front/src/Feature/Header/Header.tsx
@@ -9,7 +9,7 @@ import S from './Header.styled';
 import HeaderMenuList from './HeaderMenuList';
 import Palette from '../../lib/css/Pallete';
 import P from '../../shared/P/P';
-
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 
 const SearchIcon = styled(FaSearch)`
   color: ${Palette.Gray500};
@@ -21,42 +21,41 @@ function Header() {
   const [cookie] = useCookies(['user_id']);
 
   return (
-    <S.Nav>
-      <S.NavHeader>
-        <S.NavLeft>
-          <Link to="/">
+    <Navbar>
+      <Container>
+        <Navbar.Brand>
+          <Nav.Link href="/">
             <img src={'../../../assets/logo/logo.svg'}></img>
-          </Link>
-        </S.NavLeft>
-        <S.NavCenter>
-          <S.MenuLink to="#">위스키 찾기</S.MenuLink>
-          <S.MenuMargin></S.MenuMargin>
-          <S.MenuMargin></S.MenuMargin>
-          {/* <SearchIcon></SearchIcon>
+          </Nav.Link>
+        </Navbar.Brand>
+        <Nav>
+            <Nav.Link href="#">위스키 찾기</Nav.Link>
+            {/* <S.MenuMargin></S.MenuMargin>
+          <S.MenuMargin></S.MenuMargin> */}
+            {/* <SearchIcon></SearchIcon>
           <S.SearchInput></S.SearchInput> */}
-          </S.NavCenter>
-       
-        <S.NavRight>
-          {!cookie.user_id && (
-            <> 
-              <Link to="/login">
-                <S.MenuBtn>
-                  <P>로그인</P>
-                </S.MenuBtn>
-              </Link>
-              <Link to="/signup">
-                <S.SignUpBtn>
-                  <P>회원가입</P>
-                </S.SignUpBtn>
-              </Link>
-            </>
-          )}
-          {cookie.user_id && (
-            <HeaderMenuList></HeaderMenuList>
-          )}
-        </S.NavRight>
-      </S.NavHeader>
-    </S.Nav>
+          </Nav>
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+          <Navbar.Text>
+            {!cookie.user_id && (
+              <>
+                <Link to="/login">
+                  <S.MenuBtn>
+                    <P>로그인</P>
+                  </S.MenuBtn>
+                </Link>
+                <Link to="/signup">
+                  <S.SignUpBtn>
+                    <P>회원가입</P>
+                  </S.SignUpBtn>
+                </Link>
+              </>
+            )}
+            {cookie.user_id && <HeaderMenuList></HeaderMenuList>}
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
